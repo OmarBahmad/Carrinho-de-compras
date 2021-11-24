036029c07ec701d5b165206095d8e9198cbb123e
OmarBahmad <omarbahmad@gmail.com>
2021-11-24 19:52:06 -0300

    Add Context

---
+++ contexts/CartContext/index.js
@@ -0,0 +1,7 @@
+import { createContext } from "react";
+
+export const CartContext = createContext({});
+
+export const CartProvider = ({ children }) => {
+  return <CartContext.Provider>{children}</CartContext.Provider>;
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,23 @@
 import { GlobalStyle } from "../styles/globals";
 import ShoppingCart from "./index";
 import Head from "next/head";
+import { CartProvider } from "../contexts/CartContext";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <ShoppingCart />
+      <CartProvider>
+        <GlobalStyle />
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <ShoppingCart />
+      </CartProvider>
     </>
   );
 }
