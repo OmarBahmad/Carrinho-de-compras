4de39be35b17a46251fb573c79cf7352bd2861bf
OmarBahmad <omarbahmad@gmail.com>
2021-11-25 08:43:49 -0300

    cart page init

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,12 @@
 import React from "react";
 import NavBar from "../src/NavBar";
 import ShopList from "../src/ShopList";
-import Cart from "../src/Cart";
 
 const ShoppingCart = () => {
   return (
     <>
       <NavBar />
       <ShopList />
-      <Cart />
     </>
   );
 };
